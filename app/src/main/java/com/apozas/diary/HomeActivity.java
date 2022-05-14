eb44507780daaaedb6d6f8536b60ab58632054f3
Alleyoops <hp917@qq.com>
2022-05-14 18:46:42 +0800

    merge

---
+++ app/src/main/java/com/apozas/diary/HomeActivity.java
@@ -0,0 +1,41 @@
+package com.apozas.diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.epidemicbigdata.MainActivity2;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        //test
+        Button btn1 = findViewById(R.id.test1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(HomeActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn2 = findViewById(R.id.test2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(HomeActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
